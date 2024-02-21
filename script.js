f108f665550d12f8ab745d17d4782c7064f07208
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-02-21 18:51:18 +0530

    Functions

---
+++ script.js
@@ -0,0 +1,82 @@
+//let a;
+//console.log(a);
+
+// let ioi = [1,2,"ioistud", true];
+// console.log(ioi);
+
+// let user1 = {
+//     Name : 'Akanksha',
+//     age : '19'
+
+// };
+// console.log(user1);
+
+// // If else, Switch 
+//let bank = 10;
+// if(bank >= 10){
+//     console.log("Allow");
+// }else{
+//     console.log("Not Allow");
+// }
+
+// switch(bank){
+//     case 10: 
+//      console.log("Allowed");
+//     break;
+//     case 9:
+//         console.log("Not allowed");
+//         break;
+
+// }
+
+// let rain = true;
+// rain? console.log("it's rannnnnn"):console.log("sdhjsbjbabv");
+// let n=10;
+// for(let i=0;i<n;i++){
+//     console.log(i)
+// }
+// let i = 1;
+// do {
+//     console.log("Hello");
+//     i++;
+// } while (i < n);
+// Function
+
+// function Samriddhi(){
+//     let mob = 1234567890;
+//     console.log("Samriddhi Sharma", mob);
+// }
+// Samriddhi();
+
+// function Samriddhi(){
+//     let mob = 1234567890;
+//     let name = "Akanksha";
+//     return name + " " + mob;
+// }
+// let det = Samriddhi();
+// console.log(det);
+
+// function ioi(prm1,prm2) {
+//     let cal = prm2 + prm1 * 0.8;
+//     return cal;
+// }
+// let printake = ioi(80, 90);
+// console.log(printake);
+
+
+// function pwioi(arr){
+//     let sum = 0;
+//     for(let i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+//     return sum;
+// }
+// let data = [1,2,3,4,5]
+// console.log(pwioi(data));
+
+let bank = 100000;
+   if(bank>=100000){
+    console.log("Allow Transaction");
+   }else{
+    console.log("No Transaction Allowed");
+   } 
