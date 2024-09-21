e6ff4df9360c108d4aa551b2cd620f4ffeccf069
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-09-21 23:47:10 +0530

    JAVA

---
+++ continuestatement.java
@@ -0,0 +1,16 @@
+public class continuestatement {
+    public static void main (String [] args){
+        for (int i = 1; i<=5;i++){
+            for(int j = 1; j<=5; j++){
+                if (i == 3 && j>=2 && j<4){
+                    continue;
+                }
+                    System.out.println(j);
+                }               
+            }
+           System.out.println(); 
+    }
+
+} 
+
+
---
+++ pro1A.java
@@ -0,0 +1,6 @@
+public class pro1A {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+    
+}
